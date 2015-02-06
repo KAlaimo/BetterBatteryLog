dcfb98e85880c4ad87818a16fbe0f01b742fd592
Kristen Alaimo <kristen.alaimo@gmail.com>
2015-02-05 20:37:35 -0800

    Dev Update 12
    Wired up DatePickers.

--- app/src/main/java/com/littleandroid/betterbatterylog/BatteryEntryActivity.java
+++ app/src/main/java/com/littleandroid/betterbatterylog/BatteryEntryActivity.java
@@ -1,6 +1,7 @@
 package com.littleandroid.betterbatterylog;
 
 import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Intent;
@@ -15,25 +16,71 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
 import org.json.JSONException;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 
-public class BatteryEntryActivity extends ActionBarActivity {
+public class BatteryEntryActivity extends ActionBarActivity implements OnDateSetListener {
 
     private static final String TAG = "BBL-BatteryEntryActivity";
 
+    public static final String TAG_INSTALL_PICKER = "installDatePicker";
+    public static final String TAG_DIED_PICKER = "diedDatePicker";
     public static final String SIDE_EXTRA = "setSideRequest";
     public static final String JSON_EXTRA = "jsonString";
+    public static final String DATE_KEY = "logDate";
 
+    private TextView mInstallDateTV;
+    private TextView mDiedDateTV;
     private Switch mLeftRightSwitch;
     private BatteryEntry mBattery;
+    private static String mDatePickerTag;
 
+    public static class DatePickerFragment extends DialogFragment{
+
+        private DatePickerDialog.OnDateSetListener onDateSetListener;
+
+        static DatePickerFragment newInstance(Date date, DatePickerDialog.OnDateSetListener onDateSetListener) {
+            DatePickerFragment pickerFragment = new DatePickerFragment();
+            pickerFragment.setOnDateSetListener(onDateSetListener);
+
+            //Pass the date in a bundle.
+            Bundle bundle = new Bundle();
+            bundle.putSerializable(DATE_KEY, date);
+            pickerFragment.setArguments(bundle);
+            return pickerFragment;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            super.onCreateDialog(savedInstanceState);
+
+            Date initialDate = (Date) getArguments().getSerializable(DATE_KEY);
+            int[] yearMonthDay = ymdTripleFor(initialDate);
+            DatePickerDialog dialog = new DatePickerDialog(getActivity(), onDateSetListener, yearMonthDay[0], yearMonthDay[1],
+                    yearMonthDay[2]);
+            return dialog;
+        }
+
+        private void setOnDateSetListener(DatePickerDialog.OnDateSetListener listener) {
+            this.onDateSetListener = listener;
+        }
+
+        private int[] ymdTripleFor(Date date) {
+            Calendar cal = Calendar.getInstance(Locale.US);
+            cal.setTime(date);
+            return new int[]{cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)};
+        }
+    }
 
 
     @Override
@@ -71,6 +118,43 @@ public class BatteryEntryActivity extends ActionBarActivity {
         // Get Side value from Intent extra and initialize mBattery.
         Side side = Side.values()[getIntent().getIntExtra(SIDE_EXTRA, Side.LEFT.ordinal())];
         mBattery = new BatteryEntry(side);
+
+        // Setup install Date TextView
+        mInstallDateTV = (TextView) findViewById(R.id.installDateTextView);
+
+        // Setup died Date TextView
+        mDiedDateTV = (TextView) findViewById(R.id.diedDateTextView);
+
+        // Setup install date ImageButton.
+        ImageButton installImageButton = (ImageButton) findViewById(R.id.pickInstallDateButton);
+        installImageButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Date d = mBattery.getInstallDate();
+                if(d == null) {
+                    d = new Date();
+                }
+                DatePickerFragment frag = DatePickerFragment.newInstance(d, BatteryEntryActivity.this);
+                mDatePickerTag = TAG_INSTALL_PICKER;
+                frag.show(getFragmentManager(), mDatePickerTag);
+            }
+        });
+
+        // Setup died date ImageButton.
+        ImageButton diedImageButton = (ImageButton) findViewById(R.id.pickDiedDateButton);
+        diedImageButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Date d = mBattery.getDiedDate();
+                if(d == null) {
+                    d = new Date();
+                }
+                DatePickerFragment frag = DatePickerFragment.newInstance(d, BatteryEntryActivity.this);
+                mDatePickerTag = TAG_DIED_PICKER;
+                frag.show(getFragmentManager(), mDatePickerTag);
+            }
+        });
+
         // Setup the Left-Right switch
         mLeftRightSwitch = (Switch) findViewById(R.id.leftRightSwitch);
         setSwitch(side);
@@ -78,14 +162,28 @@ public class BatteryEntryActivity extends ActionBarActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked) {
-                    setSwitch(Side.RIGHT);
+                    mLeftRightSwitch.setBackgroundColor(getResources().getColor(R.color.color_right_red));
                 }
                 else {
-                    setSwitch(Side.LEFT);
+                    mLeftRightSwitch.setBackgroundColor(getResources().getColor(R.color.color_left_blue));
                 }
             }
         });
 
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        GregorianCalendar cal = new GregorianCalendar(year, monthOfYear, dayOfMonth);
+        Log.i(TAG, "Tag: " + mDatePickerTag);
+        if(mDatePickerTag == TAG_INSTALL_PICKER) {
+            mBattery.setInstallDate(cal.getTime());
+            mInstallDateTV.setText(DateFormat.getDateInstance(DateFormat.SHORT).format(mBattery.getInstallDate()));
+        }
+        else {
+            mBattery.setDiedDate(cal.getTime());
+            mDiedDateTV.setText(DateFormat.getDateInstance(DateFormat.SHORT).format(mBattery.getDiedDate()));
+        }
     }
 
     private void setSwitch(Side side) {
